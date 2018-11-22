d039e68022cdb43c62fc5ab6a9f3c7b0eef36499
jiangwei <jiangwei_android@163.com>
2018-11-22 23:45:36 +0800

    [可见性ing]

--- app/src/main/java/com/example/jiangwei/kotlinlearn/面向对象/可见性.kt
+++ app/src/main/java/com/example/jiangwei/kotlinlearn/面向对象/可见性.kt
@@ -5,7 +5,7 @@ package com.example.jiangwei.kotlinlearn.面向对象
  *     author : jiangwei
  *     e-mail : jiangwei_android@163.com
  *     time   : 2018/11/21
- *     desc   :
+ *     desc   : 可见性和java类似，多了一个包内可见，默认是public
  *     version: 1.0
  * </pre>
  */
